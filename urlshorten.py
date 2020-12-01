0e146589e11d1f14967dff7f40c4a318068b00cf
rambius <rambiusparkisanius@gmail.com>
2020-12-01 04:54:46 +0000

    added db insert

--- urlshorten.py
+++ urlshorten.py
@@ -2,6 +2,7 @@
 
 import base64
 import hashlib
+import sqlite3
 
 def to_raw(s):
   return b"{s}"
@@ -13,13 +14,24 @@ def generate_hash(url):
   return shad
 
 def encode_hash(shad):
-  return base64.b32encode(shad.digest())
+  return base64.b64encode(shad.digest())
+
+def key_from_url(url, length=10):
+  hash = generate_hash(url)
+  ehash = encode_hash(hash)
+  return ehash[:length]
+
+def add_url(url):
+  key = key_from_url(url)
+  conn = sqlite3.connect("sql/urls.db")
+  curs = conn.cursor()
+  sql = "insert into URLS values ('" + key + "', '" + url + "');"
+  print(sql)
+  conn.close()
 
 def main():
   url = "https://www.educative.io/courses/grokking-the-system-design-interview/m2ygV4E81AR"
-  shad = generate_hash(url)
-  enc = encode_hash(shad)
-  print(enc)
+  add_url(url)
 
 if __name__ == '__main__':
   main()
